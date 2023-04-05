4fc8246fd5c2320daa71c79b6cfa74d31ce584bd
aprnald74 <a010313813321@gmail.com>
2023-04-05 17:06:54 +0900

    :white_check_mark:

---
+++ src/kr/hs/dgsw/java/aprnald/Score.java
@@ -0,0 +1,24 @@
+package kr.hs.dgsw.java.aprnald;
+
+public class Score {
+
+    void score(int score) {
+
+        if (score > 89) {
+            System.out.println("수");
+        } else if (score > 79) {
+            System.out.println("우");
+        } else if (score > 69) {
+            System.out.println("미");
+        } else if (score > 59) {
+            System.out.println("양");
+        } else {
+            System.out.println("가");
+        }
+
+    }
+    public static void main(String[] args) {
+        Score score = new Score();
+        score.score(6);
+    }
+}
