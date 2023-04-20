b3f88dd76a5a33287f4fff457f9ee59796553810
aprnald74 <a010313813321@gmail.com>
2023-04-20 23:50:42 +0900

    :technologist:

---
+++ src/kr/hs/dgsw/java/inherit4/Animal.java
@@ -0,0 +1,15 @@
+package kr.hs.dgsw.java.inherit4;
+
+public class Animal {
+
+    protected String name;
+
+    public void move() {
+        System.out.println("움직인다.");
+    }
+
+    public void die() {
+        System.out.println("죽는다.");
+    }
+
+}
---
+++ src/kr/hs/dgsw/java/inherit4/Minus.java
@@ -0,0 +1,18 @@
+package kr.hs.dgsw.java.inherit4;
+
+public class Minus extends Adder {
+
+    @Override
+    public int calculate() {
+        return op1 - op2;
+    }
+
+    public static void main(String[] args) {
+        Adder adder = new Minus();
+
+        adder.setOp1(7);
+        adder.setOp2(4);
+
+        System.out.println(adder.calculate());
+    }
+}
